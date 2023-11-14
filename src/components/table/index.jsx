8b1bc23604f77ecfa01ceaa863c594b0089ef1d6
Solarin <solarinjohnson@gmail.com>
2023-11-14 03:35:47 +0100

    commit

---
+++ src/components/table/index.jsx
@@ -0,0 +1,11 @@
+import "./table.scss";
+export default function Table() {
+  return (
+    <div className="table-items">
+      <div className="table-items-name">Create home page Puzzle</div>
+      <div className="table-items-priority">
+        <i class="fa-solid fa-fire-flame-curved"></i>
+      </div>
+    </div>
+  );
+}
--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 import Navbar from "../../components/navigation";
 import "./dashboard.scss";
+import Table from "../../components/table";
 export default function Dashboard() {
   return (
     <div className="dashboard">
       <Navbar />
       <div className="dashboard-container">
         <UserCard />
+        <TaskList />
       </div>
     </div>
   );
@@ -26,7 +28,13 @@ export function UserCard({ progress = 65 }) {
       }
     });
   }, []);
-
+  const [activeFilter, setActiveFilter] = useState(["active-filter", ""]);
+  const filterImportant = () => {
+    setActiveFilter(["", "active-filter"]);
+  };
+  const filterAll = () => {
+    setActiveFilter(["active-filter", ""]);
+  };
   return (
     <div
       className={browserWidth > 500 ? "usercard" : "mobile_usercard usercard"}
@@ -40,15 +48,58 @@ export function UserCard({ progress = 65 }) {
         </div>
         <div className="usercard-game">
           <div className="usercard-points">
-            <i class="fa-solid fa-trophy"></i>
+            <i className="fa-solid fa-trophy"></i>
             <span>18</span>
           </div>
           <div className="usercard-streak">
-            <i class="fa-solid fa-fire"></i> <span>3</span>
+            <i className="fa-solid fa-fire"></i> <span>3</span>
           </div>
         </div>
       </div>
-      <div className="usercard-tabs"></div>
+      <div className="usercard-tabs">
+        <div className="usercard-tabs-title">Tasks</div>
+        <div className="usercard-tabs-filter">
+          <div className={`${activeFilter[0]} all`} onClick={filterAll}>
+            <span>All</span>
+            <span>8</span>
+          </div>
+          <div
+            className={`${activeFilter[1]} important`}
+            onClick={filterImportant}
+          >
+            <span>Important</span>
+            <span>2</span>
+          </div>
+        </div>
+        <div className="usercard-tabs-new">
+          <span>New Task</span>
+          <i className="fa-solid fa-plus"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function TaskList() {
+  const currentDate = new Date();
+  const options = { day: "2-digit", month: "short", year: "numeric" };
+  var todayDate = currentDate.toLocaleDateString("en-UK", options);
+  var todayDay = currentDate.toLocaleDateString("en-US", { weekday: "long" });
+
+  return (
+    <div className="table">
+      <div className="table-header">
+        <div className="table-title">
+          <span>Today</span>
+          <span>3</span>
+        </div>
+        <div className="table-date">
+          <span>{todayDay}</span> <span>{todayDate}</span>
+        </div>
+      </div>
+      <div className="table-container">
+        <Table />
+      </div>
     </div>
   );
 }
