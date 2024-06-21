e63c35411ea10fb67d997de19f64dc0e93ea0084
danieltonad <soladan2000@gmail.com>
2024-06-21 00:42:45 -0900

    create task endpoint

--- server/controller.py
+++ server/controller.py
@@ -1,11 +1,12 @@
 from database import todos
 from model import Todo
-from fast
+from fastapi.responses import JSONResponse
+from fastapi import status
 
 async def new_todo(todo: Todo):
     try:
-        todos.put(todo)
-        return {'message': "Todo Added!", "data": todo}
+        new = todos.insert(todo)
+        return JSONResponse(content={'message': "Todo Added!", "data": new}, status_code=status.HTTP_201_CREATED)
     except Exception as e:
         print(e)
-        return {'message': "Todo Added!", "data": todo}
+        return JSONResponse(content={'message': "Unable to add todo"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- server/main.py
+++ server/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from model import Todo
+from controller import new_todo
 
 app = FastAPI(title="Todos Sample")
 
 
 
-@app.get('/')
+@app.get('/', tags=['Todos'])
 async def get_todo():
     return []
 
+@app.post('/add', tags=['Add Todo'])
+async def add_todo(data: Todo):
+    data = dict(data)
+    print(data: Todo)
+    return await new_todo(data)
