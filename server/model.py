2ac931602c24add0f612c2fb105e0a1b8e49aafe
danieltonad <soladan2000@gmail.com>
2024-06-21 02:27:39 -0900

    fix: module 'orjson' has no attribute 'JSONEncoder'

--- server/model.py
+++ server/model.py
@@ -11,7 +11,7 @@ class UpdateTodo(BaseModel):
     favourite: bool = None
     description: str = None
     
-    @model_validator(mode='before')
+    @model_validator(mode='after')
     def check_non_empty(cls, values):
         if not values.get('description') and not values.get('title') and not values.get('favourite'):
             raise ValueError('At least one of "title", "description" or "favourite" must be provided and cannot be None.')
