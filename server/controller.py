e8eb23aef546e3c76af892d38476c3c8e7faec2b
danieltonad <soladan2000@gmail.com>
2024-06-21 02:55:53 -0900

    fix: downgrade fastapi version --rollback-root_validator

--- server/controller.py
+++ server/controller.py
@@ -1,32 +1,32 @@
 from database import todos
 from model import Todo, UpdateTodo
-from fastapi.responses import ORJSONResponse
+from fastapi.responses import JSONResponse
 from fastapi import status
 from schema import todo_serializer, todos_serializer
 
 async def get_todos():
     try:
         all = todos.fetch()._items
-        return ORJSONResponse(content={'message': "Todos", "data": todos_serializer(all)}, status_code=status.HTTP_200_OK) if todos else ORJSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'Todos Empty!'})
+        return JSONResponse(content={'message': "Todos", "data": todos_serializer(all)}, status_code=status.HTTP_200_OK) if todos else JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'Todos Empty!'})
     except Exception as e:
         print(e)
-        return ORJSONResponse(content={'message': "Unable to get todo"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return JSONResponse(content={'message': "Unable to get todo"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 async def new_todo(todo: Todo):
     try:
         new = todos.insert(todo)
-        return ORJSONResponse(content={'message': "Todo Added!", "data": todo_serializer(new)}, status_code=status.HTTP_201_CREATED)
+        return JSONResponse(content={'message': "Todo Added!", "data": todo_serializer(new)}, status_code=status.HTTP_201_CREATED)
     except Exception as e:
         print(e)
-        return ORJSONResponse(content={'message': "Unable to add todo"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return JSONResponse(content={'message': "Unable to add todo"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 async def find_todo(id: str):
     try:
         todo = todos.get(id)
-        return ORJSONResponse(content={'message': "Todo Found!", "data": todo_serializer(todo)}, status_code=status.HTTP_200_OK) if todo else ORJSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'Invalid Todo Id!'})
+        return JSONResponse(content={'message': "Todo Found!", "data": todo_serializer(todo)}, status_code=status.HTTP_200_OK) if todo else JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'Invalid Todo Id!'})
     except Exception as e:
         print(e)
-        return ORJSONResponse(content={'message': "Unable to find todo"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return JSONResponse(content={'message': "Unable to find todo"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     
 async def delete_todo(id: str):
@@ -34,13 +34,13 @@ async def delete_todo(id: str):
         todo = todos.get(id)
         
         if not todo:
-            return ORJSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'Invalid Todo Id!'})
+            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'Invalid Todo Id!'})
         
         todos.delete(id)
-        return ORJSONResponse(content={'message': "Todo Deleted!"}, status_code=status.HTTP_200_OK)
+        return JSONResponse(content={'message': "Todo Deleted!"}, status_code=status.HTTP_200_OK)
     except Exception as e:
         print(e)
-        return ORJSONResponse(content={'message': "Unable to delete todo"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return JSONResponse(content={'message': "Unable to delete todo"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     
 async def update_todo(id: str, todo: UpdateTodo):
@@ -49,11 +49,11 @@ async def update_todo(id: str, todo: UpdateTodo):
         todo = todos.get(id)
         
         if not todo:
-            return ORJSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'Invalid Todo Id!'})
+            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'message': 'Invalid Todo Id!'})
         
         todos.update(updates=payload, key=id)
-        return ORJSONResponse(content={'message': "Todo Updated!"}, status_code=status.HTTP_200_OK)
+        return JSONResponse(content={'message': "Todo Updated!"}, status_code=status.HTTP_200_OK)
     
     except Exception as e:
         print(e)
-        return ORJSONResponse(content={'message': "Unable to update todo"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return JSONResponse(content={'message': "Unable to update todo"}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- server/model.py
+++ server/model.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, model_validator, root_validator
 from typing import Optional
 
 class Todo(BaseModel):
@@ -11,7 +11,7 @@ class UpdateTodo(BaseModel):
     favourite: bool = None
     description: str = None
     
-    @model_validator(mode='after')
+    @root_validator(pre=True)
     def check_non_empty(cls, values):
         if not values.get('description') and not values.get('title') and not values.get('favourite'):
             raise ValueError('At least one of "title", "description" or "favourite" must be provided and cannot be None.')
